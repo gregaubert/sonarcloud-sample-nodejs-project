d75d7c0dee8cc46c4a3a8c22a8ebbacf904ac2c6
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2020-11-13 08:47:42 +0100

    new changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 // main app entry point
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
+
+const test = "vide";
+const test3 = "vide";
+const test5 = "vide";
+const test6 = "vide";
+const test7 = "vide";
 
 platformBrowserDynamic().bootstrapModule(AppModule);
